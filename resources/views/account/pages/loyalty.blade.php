e9190fa150964f4fdcfbbe40a19e89274523d1ea
timurlatypov <timur.latypov@gmail.com>
2018-08-12 12:52:59 +0300

    4

--- resources/views/account/pages/loyalty.blade.php
+++ resources/views/account/pages/loyalty.blade.php
@@ -6,6 +6,6 @@
     <p>Ваша персональная скидка: {{ $user->loyalty }}%</p>
     <p>Общая сумма заказов: {{ $user->totalSum() }} &#x20BD;</p>
 
-    <p>Hello W</p>
+    <p>Hello World</p>
 @endsection
 
