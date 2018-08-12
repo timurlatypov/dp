5e13d7cc853e280501e889821cd7eeb39819b5d9
timurlatypov <timur.latypov@gmail.com>
2018-08-12 17:08:01 +0300

    222

--- config/app.php
+++ config/app.php
@@ -151,7 +151,7 @@ return [
          * Package Service Providers...
          */
 	    Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
-	    Barryvdh\Debugbar\ServiceProvider::class,
+	    //Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -211,7 +211,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 	    'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
-	    'Debugbar' => Barryvdh\Debugbar\Facade::class
+	    //'Debugbar' => Barryvdh\Debugbar\Facade::class
     ],
 
 ];
