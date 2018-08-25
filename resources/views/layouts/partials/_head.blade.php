2469c15ab7e5fc3e757c52c94383ec300fefea0b
timurlatypov <timur.latypov@gmail.com>
2018-08-26 01:54:54 +0300

    123

--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -33,3 +33,4 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+    @include('layouts.partials._metrika')
