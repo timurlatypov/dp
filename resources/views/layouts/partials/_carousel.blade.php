5540221d1951f7ad2a420c094258746c597b1105
Timur Latypov <timur.latypov@gmail.com>
2018-11-26 00:25:24 +0300

    carousel arrows color changes

--- resources/views/layouts/partials/_carousel.blade.php
+++ resources/views/layouts/partials/_carousel.blade.php
@@ -32,11 +32,11 @@
 
 
 
-            <div class="carousel-item" style="background-color: #e7a9b5">
+            <div class="carousel-item" style="background-color: #000000">
                 <a href="https://doctorproffi.ru/discounts" class="banner-mobile h-100">
                     <div class="row h-100">
                         <div class="col-12 h-100 align-self-center ">
-                            <div class="h-100 carousel-bg-image" style="background-image: url('/storage/carousel/mobile/mobile-50percent-and-filler-mask-1000x700.jpg')">
+                            <div class="h-100 carousel-bg-image" style="background-image: url('/storage/carousel/mobile/mobile-black-friday.jpg')">
                             </div>
                         </div>
                     </div>
@@ -44,14 +44,14 @@
                 <a href="https://doctorproffi.ru/discounts" class="banner-tablet h-100">
                     <div class="row h-100">
                         <div class="col-12 h-100 align-self-center ">
-                            <div class="h-100 carousel-bg-image" style="background-image: url('/storage/carousel/tablet/tablet-50percent-and-filler-mask-1000x600.jpg')">
+                            <div class="h-100 carousel-bg-image" style="background-image: url('/storage/carousel/tablet/tablet-black-friday.jpg')">
                             </div>
                         </div>
                     </div>
                 </a>
                 <a href="https://doctorproffi.ru/discounts" class="banner-desktop h-100">
                     <div class="container-carousel h-100 px-0">
-                        <div class="row h-100 carousel-bg-image" style="background-image: url('/storage/carousel/desktop/desktop-50percent-and-filler-mask-2100x600.jpg')">
+                        <div class="row h-100 carousel-bg-image" style="background-image: url('/storage/carousel/desktop/desktop-black-friday.jpg')">
                             <div class="col-12 align-self-center px-5">
 
                             </div>
@@ -83,6 +83,33 @@
                     <div class="row h-100 carousel-bg-image" style="background-image: url('/storage/carousel/desktop/desktop-first-order-10percent.jpg')">
                         <div class="col-12 align-self-center px-5">
 
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="carousel-item " style="background-color: #ecedf1">
+            <a href="https://doctorproffi.ru/brand/professional-solutions" class="banner-mobile h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100 align-self-center ">
+                        <div class="h-100 carousel-bg-image" style="background-image: url('/storage/carousel/mobile/mobile-professional-solutions.jpg')">
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="https://doctorproffi.ru/brand/professional-solutions" class="banner-tablet h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100 align-self-center ">
+                        <div class="h-100 carousel-bg-image" style="background-image: url('/storage/carousel/tablet/tablet-professional-solutions.jpg')">
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="https://doctorproffi.ru/brand/professional-solutions" class="banner-desktop h-100">
+                <div class="container-carousel h-100 px-0">
+                    <div class="row h-100 carousel-bg-image" style="background-image: url('/storage/carousel/desktop/desktop-professional-solutions.jpg')">
+                        <div class="col-12 align-self-center px-5">
                         </div>
                     </div>
                 </div>
