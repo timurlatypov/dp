a93f68fe3a5ee5a22c0bde805240b709c81d43e0
Timur Latypov <timur.latypov@skillbox.ru>
2021-05-24 11:46:01 +0300

    Add facebook verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta name="facebook-domain-verification" content="lteht5c11k3xsym3kk0qm00izgq7zv" />
     @include('layouts.partials._head')
     @stack('meta')
     @stack('share')
