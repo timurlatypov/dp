4b7f6e4805df6afce552e38a7f55384f39599553
Timur Latypov <timur.latypov@gmail.com>
2022-01-30 02:48:11 +0300

    New FB Pixel

--- resources/views/layouts/partials/_facebook_pixel.blade.php
+++ resources/views/layouts/partials/_facebook_pixel.blade.php
@@ -6,12 +6,14 @@
         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
         n.queue=[];t=b.createElement(e);t.async=!0;
         t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '2151538838446479');
+    fbq('init', '1105711580264178');
     fbq('track', 'PageView');
 </script>
-<noscript><img height="1" width="1" style="display:none"
-               src="https://www.facebook.com/tr?id=2151538838446479&ev=PageView&noscript=1"
-    /></noscript>
+<noscript>
+    <img height="1" width="1"
+         src="https://www.facebook.com/tr?id=1105711580264178&ev=PageView
+&noscript=1"/>
+</noscript>
 <!-- End Facebook Pixel Code -->
