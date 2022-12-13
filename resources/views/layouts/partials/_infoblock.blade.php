a1daa5132ca57625e578d5c5ef9fb2e2db12e245
Timur Latypov <timur.latypov@gmail.com>
2022-12-13 22:22:23 +0300

    Section rename

--- resources/views/layouts/partials/_infoblock.blade.php
+++ resources/views/layouts/partials/_infoblock.blade.php
@@ -3,13 +3,10 @@
         <div class="col-12 pt-3">
             <div class="card">
                 <div class="card-header card-header-rose text-center">
-                    <h3 class="card-title mb-1 mt-0">Мы рекомендуем!</h3>
+                    <h3 class="card-title mb-1 mt-0">Хиты продаж</h3>
                 </div>
                 <div class="card-body d-flex flex-wrap card-col-12">
                     @each('layouts.partials.product.card', $recommend, 'product')
-                </div>
-                <div class="footer text-center pb-3">
-                    <a href="/brand/sets" class="btn btn-rose">Все наборы</a>
                 </div>
             </div>
         </div>
