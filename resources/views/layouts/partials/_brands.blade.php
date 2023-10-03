abfa06d4735d0fc3be7ba1f194fc024befa092f0
Timur Latypov <timur.latypov@gmail.com>
2023-10-03 23:18:50 +0300

    fix: change corpolibero logo in brands block to another one.

--- resources/views/layouts/partials/_brands.blade.php
+++ resources/views/layouts/partials/_brands.blade.php
@@ -4,11 +4,11 @@
     </div>
     <div class="row p-0 p-md-5">
         <div class="d-flex align-items-center justify-content-around w-100 brands-flex">
-            <a href="/brand/mene-and-moy-system"><div class="brands-preview"><img src="/storage/brands/mene-and-moy-system.png" alt="Логотип"></div></a>
-            <a href="/brand/mycli"><div class="brands-preview"><img src="/storage/brands/MyCLI.png" alt="Логотип"></div></a>
-            <a href="/brand/corpolibero"><div class="brands-preview"><img src="/storage/brands/corpolibero.png" alt="Логотип"></div></a>
-            <a href="/brand/professional-solutions"><div class="brands-preview"><img src="/storage/brands/professional-solutions.png" alt="Логотип"></div></a>
-            <a href="/brand/b-selfie"><div class="brands-preview"><img src="/storage/brands/bselfie.png" alt="Логотип"></div></a>
+            <a href="/brand/mene-and-moy-system"><div class="brands-preview"><img src="/storage/brands/mene-and-moy-system.png" alt="Логотип Mene&Moy System"></div></a>
+            <a href="/brand/mycli"><div class="brands-preview"><img src="/storage/brands/MyCLI.png" alt="Логотип MyCLI"></div></a>
+            <a href="/brand/biomatrix"><div class="brands-preview"><img src="/storage/brands/biotime.png" alt="Логотип Biotime"></div></a>
+            <a href="/brand/professional-solutions"><div class="brands-preview"><img src="/storage/brands/professional-solutions.png" alt="Логотип Professional Solutions"></div></a>
+            <a href="/brand/b-selfie"><div class="brands-preview"><img src="/storage/brands/bselfie.png" alt="Логотип B Selfie"></div></a>
         </div>
     </div>
 </div>
